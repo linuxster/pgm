8bb9d4baeb505444977c18e1248c565ade2773c1
Dan F-M <danfm@nyu.edu>
2012-04-06 12:00:41 -0400

    slight refactor now that I know what I'm doing

--- ps5/lda.py
+++ ps5/lda.py
@@ -13,114 +13,132 @@ __all__ = ["LDA"]
 import numpy as np
 
 class LDA(object):
-    def __init__(self, fn=None, alpha=None, beta=None, rstate=None):
-        assert (fn is None and alpha is not None and beta is not None) or \
-               (fn is not None and alpha is None and beta is None)
-
-        self._words = []
-        if fn is not None:
-            f = open(fn, "r")
-            ntopics = int(f.readline())
-            alpha = np.array(f.readline().split(), dtype=float)
-            assert len(alpha) == ntopics
-            beta = []
-            for line in f:
-                c = line.split()
-                self._words.append(c[0])
-                beta.append([v for v in c[1:]])
-            f.close()
-            beta = np.array(beta, dtype=float).T
-
-        self.alpha = alpha/np.sum(alpha)
-        self.beta  = beta/(np.sum(beta, axis=1)[:, None])
-
+    def __init__(self, fn=None):
         # I like to let my samplers own their own random number generators
         # so here I'm initializing on from numpy.
         self._random = np.random.mtrand.RandomState()
-        try:
-            self._random.set_state(rstate)
-        except:
-            pass
 
-    @property
-    def words(self):
+        # Read alpha, beta and the word list in from a .ready file.
+        f = open(fn, "r")
+        ntopics = int(f.readline())
+        alpha = np.array(f.readline().split(), dtype=float)
+        assert len(alpha) == ntopics
+        beta = []
+        self._words = []
+        for line in f:
+            c = line.split()
+            self._words.append(c[0])
+            beta.append([v for v in c[1:]])
+        f.close()
+        beta = np.array(beta, dtype=float).T
+
+        # Make sure that alpha and beta are properly normalized.
+
+        # `alpha` should sum to 1. NOTE: `alpha.shape = (ntopics,)`.
+        self.alpha = alpha/np.sum(alpha)
+
+        # For a particular topic, `i`, `sum(beta[i,:]) == 1`.
+        # NOTE: `beta.shape = (ntopics, nwords)`
+        self.beta  = beta/(np.sum(beta, axis=1)[:, None])
+
+        # The word distribution in these particular documents is just one
+        # count of each word, I think.
+        self.w = np.arange(self.beta.shape[1])
+
+        # We can initialize the topic distribution as uniform.
+        # NOTE: `theta.shape = (ntopics,)`
+        self.theta = np.ones(len(self.alpha), dtype=float)/len(self.alpha)
+
+        # The actually per-word topics are initially chosen to be random.
+        # NOTE: `z.shape = (nwords,)`
+        self.z = self.random.randint(len(self.alpha), size=len(self.w))
+
+    # @property
+    # def words(self):
+    #     """
+    #     Return the list of words that appear in the generated documents.
+
+    #     """
+    #     if len(self._words) == 0:
+    #         return self.w
+    #     r = []
+    #     for m in range(self.w.shape[0]):
+    #         r.append([])
+    #         for n in range(self.w.shape[1]):
+    #             r[-1].append(self._words[self.w[m,n]])
+    #     return r
+
+    # def initialize(self, M, N):
+    #     """
+    #     Set up the model to get ready for sampling.
+
+    #     """
+    #     self.theta = np.ones(M)/float(M)
+    #     self.z     = np.zeros([M, N], dtype=int)
+    #     self.w     = np.zeros([M, N], dtype=int)
+
+    # def generate_theta(self, M):
+    #     """
+    #     Sample the topic distribution from the Dirichlet distribution and
+    #     save its state.
+
+    #     """
+    #     self.theta = self._random.dirichlet(self.alpha, size=M)
+    #     return self.theta
+
+    # def generate_topics(self, M, N):
+    #     """
+    #     Draw topic samples from the sample distribution.
+
+    #     """
+    #     for m in range(M):
+    #         for n in range(N):
+    #             mask = self._random.multinomial(1, self.theta[m, :])
+    #             v = np.arange(self.theta.shape[1])[mask == 1]
+    #             self.z[m, n] = int(v)
+
+    # def generate_words(self, M, N):
+    #     for m in range(M):
+    #         for n in range(N):
+    #             mask = self._random.multinomial(1, self.beta[self.z[m, n],:])
+    #             v = np.arange(self.beta.shape[1])[mask == 1]
+    #             self.w[m, n] = int(v)
+
+    # def generate(self, M, N):
+    #     """
+    #     Generate `M` documents with `N` words each sampled from the LDA with
+    #     the given `alpha` and `beta` values.
+
+    #     """
+    #     self.initialize(M, N)
+    #     self.generate_theta(M)
+    #     self.generate_topics(M, N)
+    #     self.generate_words(M, N)
+
+    def sample_theta(self):
+        alpha2 = np.sum(np.sum(self.z[:,:,None] == \
+                np.arange(len(self.alpha))[None,None,:], axis=0), axis=0)
+        self.theta = self._random.dirichlet(alpha2)
+
+    def sample_topics(self):
+        pass
+
+    def gibbs_sample(self, iterations):
         """
-        If there was a list of words provided, produce the list of words that
-        appear in the generated documents.
+        Sample the conditional distribution p ( theta, z | w, alpha, beta )
+        using Gibbs sampling.
 
         """
-        if len(self._words) == 0:
-            return self.w
-        r = []
-        for m in range(self.w.shape[0]):
-            r.append([])
-            for n in range(self.w.shape[1]):
-                r[-1].append(self._words[self.w[m,n]])
-        return r
+        self.w = np.atleast_2d(np.arange(self.beta.shape[1])).T
 
-    def initialize(self, M, N):
-        """
-        Set up the model to get ready for sampling.
+        self.initialize(*self.beta.shape)
 
-        """
-        self.theta = np.ones(M)/float(M)
-        self.z     = np.zeros([M, N], dtype=int)
-        self.w     = np.zeros([M, N], dtype=int)
-
-    def sample_theta(self, M):
-        """
-        Sample the topic distribution from the Dirichlet distribution and
-        save its state.
-
-        """
-        self.theta = self._random.dirichlet(self.alpha, size=M)
-        return self.theta
-
-    def sample_topics(self, M, N):
-        """
-        Draw topic samples from the sample distribution.
-
-        """
-        for m in range(M):
-            for n in range(N):
-                mask = self._random.multinomial(1, self.theta[m, :])
-                v = np.arange(self.theta.shape[1])[mask == 1]
-                self.z[m, n] = int(v)
-
-    def sample_words(self, M, N):
-        for m in range(M):
-            for n in range(N):
-                mask = self._random.multinomial(1, self.beta[self.z[m, n],:])
-                v = np.arange(self.beta.shape[1])[mask == 1]
-                self.w[m, n] = int(v)
-
-    def generate(self, M, N):
-        """
-        Generate `M` documents with `N` words each sampled from the LDA with
-        the given `alpha` and `beta` values.
-
-        """
-        self.initialize(M, N)
-        self.sample_theta(M)
-        self.sample_topics(M, N)
-        self.sample_words(M, N)
+        for i in xrange(iterations):
+            self.sample_theta()
+            self.sample_topics()
 
 if __name__ == '__main__':
     lda = LDA(fn="data/abstract_nips21_NIPS2008_0517.txt.ready")
-    lda.generate(3, 10)
-    print [" ".join(l) for l in lda.words]
 
-    import matplotlib.pyplot as pl
-    pl.hist(lda.w.flatten(), lda.beta.shape[-1], normed=True, histtype="step", color="k")
-
-    truth = []
-    for line in open("data/abstract_nips21_NIPS2008_0517.txt"):
-        for w in line.split():
-            try:
-                truth.append(lda._words.index(w))
-            except ValueError:
-                pass
-    pl.hist(truth, lda.beta.shape[-1], normed=True, histtype="step", color="r")
-
-    pl.show()
+    # lda.gibbs_sample(1)
 
